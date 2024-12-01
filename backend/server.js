b4878da809a21815431ba6897b3a13ca56b1895b
ameay <ameay35@gmail.com>
2024-12-02 00:22:57 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const { logger } = require("./middleware/logger");
+const cors = require("cors");
 const connectDB = require("./config/connectDB");
 
 const app = express();
